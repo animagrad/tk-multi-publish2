1768a28ca0ce4e9bdae6ce577d62675a7673fc22
Yuri Meshalkin <ymesh@me.com>
2020-01-31 14:26:48 +0200

    enable register command in maya

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ class MultiPublish2(sgtk.platform.Application):
                 "dark": {"png": os.path.join(self.disk_location, "icon_256_dark.png")}
             },
         }
-        if self.engine.name != "tk-maya":
-            self.engine.register_command(menu_caption, cb, menu_options)
+        # if self.engine.name != "tk-maya":
+        self.engine.register_command(menu_caption, cb, menu_options)
 
     @property
     def base_hooks(self):
